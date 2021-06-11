126de1f629db4606e5fad881506b661dbbba375d
Daniel O'Hanlon <daniel.ohanlon@cern.ch>
2021-06-11 04:40:26 +0100

    Add some basic particle matching.

--- getPDGModes.py
+++ getPDGModes.py
@@ -4,7 +4,59 @@ from tqdm import tqdm
 
 from bs4 import BeautifulSoup
 
-def getRawModes(fileName):
+# Regex to match arbitrary excited resonances?
+# Function to build antiparticle from particle (+ vice versa?)
+
+particleNames = {
+
+r'{{\mathit X}}' : 'X',
+
+r'{{\mathit \ell}^{+}}' : 'lepton+',
+r'{{\mathit \ell}^{-}}' : 'lepton-',
+
+r'{{\mathit \tau}^{+}}' : 'tau+',
+r'{{\mathit \tau}^{-}}' : 'tau+',
+
+r'{{\mathit \nu}_{{{{\mathit \ell}}}}}' : 'nu_lepton',
+
+r'{{\mathit \pi}^{+}}' : 'pi+',
+r'{{\mathit \pi}^{-}}' : 'pi-',
+
+r'{{\mathit K}^{+}}' : 'K+',
+r'{{\mathit K}^{-}}' : 'K-',
+
+r'{{\mathit D}_{{2}}^{*}{(2460)}^{+}}' : 'D_2*(2460)+',
+r'{{\mathit D}_{{2}}^{*}{(2460)}^{-}}' : 'D_2*(2460)-',
+
+r'{{\mathit D}^{*0}}' : 'D*0',
+r'{{\overline{\mathit D}}^{*0}}' : 'anti-D*0',
+
+r'{{\mathit D}^{*+}}' : 'D*+',
+r'{{\mathit D}^{*-}}' : 'D*-',
+
+}
+
+# Add some spaces aferwards for readability
+# Probably have some better separator in future
+
+for k, v in particleNames.items():
+    particleNames[k] = f'{v} '
+
+particleRegex = re.compile("(%s)" % "|".join(map(re.escape, particleNames.keys())))
+
+def replace_particles(regex, d, text):
+
+    return regex.sub(lambda mo: d[mo.string[mo.start():mo.end()]], text)
+
+# https://stackoverflow.com/questions/15175142/how-can-i-do-multiple-substitutions-using-regex
+def multiple_replace(d, text):
+    # Create a regular expression  from the dictionary keys
+    regex = re.compile("(%s)" % "|".join(map(re.escape, d.keys())))
+
+    # For each match, look-up corresponding value in dictionary
+    return regex.sub(lambda mo: d[mo.string[mo.start():mo.end()]], text)
+
+def getJSExpansions(fileName):
 
     f = open(fileName, 'r')
 
@@ -12,13 +64,19 @@ def getRawModes(fileName):
 
     # Find JS to open *all* dropdowns
 
-    # for link in soup.find_all('a'):
-    #     js = link.get('onclick')
-    #     if js and js[:3] == 'get':
-    #         print(js)
-    #
-    # for link in soup.find_all('script'):
-    #     print(link)
+    for link in soup.find_all('a'):
+        js = link.get('onclick')
+        if js and js[:3] == 'get':
+            print(js)
+
+    for link in soup.find_all('script'):
+        print(link)
+
+def getRawModes(fileName):
+
+    f = open(fileName, 'r')
+
+    soup = BeautifulSoup(f, 'html.parser')
 
     for i, link in enumerate(soup.find_all("tr")):
 
@@ -32,6 +90,8 @@ def getRawModes(fileName):
             scripts = t.find_all("script")
 
             if len(scripts) >= 3 and 'Gamma' in str(scripts[0]):
+
+                # TODO: Compile all of these regexes.
 
                 pad = 0
                 padMatch = re.search('<td style="padding-left: ([0-9][0-9])px;">', str(t))
@@ -52,11 +112,27 @@ def getRawModes(fileName):
 
                 padPrev = pad
 
+                modes = scripts[1:-1]
+                modesLatex = []
+                for m in modes:
+                    latexWrapper = '<script id="MathJax-Element-.*" type="math/tex">(.*)</script>'
+                    match = re.search(latexWrapper, str(m))
+                    if match:
+                        modesLatex.append(match.group(1))
+
                 print('Subdecay : ', parentDecays)
 
                 print('Gamma : ', scripts[0])
                 print('Padding : ', pad)
-                print('Contents :', scripts[1:-1])
+                # print('Contents :', modes)
+                print('Contents :', modesLatex)
+
+                # Just do it for those that have a single entry here
+                # to avoid having to worry about the sub-decays for now
+                if len(modesLatex) == 1:
+                  s = replace_particles(particleRegex, particleNames, str(modesLatex[0]))
+                  print('Parsed contents', s)
+
                 print('BF : ', scripts[-1])
                 print('')
 
@@ -66,4 +142,4 @@ def getRawModes(fileName):
 
 if __name__ == '__main__':
 
-    getRawModes('testPDG.html')
+    getRawModes('testInputs/testPDG.html')
